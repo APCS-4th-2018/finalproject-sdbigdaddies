6388f2ff4c16e64a81af11093d4be3400591bcda
Nico Cereghini <nicola.cereghini@gmail.com>
2019-05-22 19:31:48 -0700

    copied Omri's PO class

--- PhysicalObject.java
+++ PhysicalObject.java
@@ -1,43 +1,80 @@
-
 /**
- * Abstract class PhysicalObject - write a description of the class here
+ * Abstract class PhysicalObject - Parent class for all objects to be rotated.
  *
- * @author (your name here)
- * @version (version number or date here)
+ * @author Omri Habot
+ * @version 1.0.0
  */
 public abstract class PhysicalObject
 {
-     protected double myMass;
-   protected double myRadius;
-   protected double MyInertiaValue;
-
-   public PhysicalObject(double m, double r)
-   {
-   
-   }
-
-   public double getMyMass()
-   {
-   }
-
-   public double getMyRadius()
-   {
-   }
-
-   public double getMyInertiaValue()
-   {
-   }
-
-   public void setMyMass(double m)
-   {
-   }
-
-   public void setMyRadius(double r)
-   {
-   }
-
-   public void setMyInertiaValue(double i)
-   {
-   }
-
+    protected double mass; //mass of the object in kilograms
+    protected double radius; //radius (half the length) of the object in meters
+    
+    /**
+    * Initializes the both mass and radius of the
+    * object with 0.
+    */
+    public PhysicalObject()
+    {
+        mass = 0;
+        radius = 0;
+    }
+    
+    /**
+    * Initializes the mass and radius of the
+    * object with the passed values.
+    * 
+    * @param myMass the mass of the object in kilograms
+    * @param myRadius the radius (half the length) of the object in meters
+    */
+    public PhysicalObject(double myMass, double myRadius)
+    {
+        mass = myMass;
+        radius = myRadius;
+    }
+    
+    /**
+    * constructor PhysicalObject - initializes the mass and radius of the
+    * object with the passed values.
+    */
+    public abstract double getInertia();
+    
+    /**
+    * Sets the new mass of the object.
+    * 
+    * @param myMass the mass of the object in kilograms.
+    */
+    public void setMass(double myMass)
+    {
+        mass = myMass;
+    }
+    
+    /**
+    * Returns the mass of the object.
+    * 
+    * @return The mass of the object in kilograms.
+    */
+    public double getMass()
+    {
+        return mass;
+    }
+    
+    /**
+    * Sets the new radius of the object.
+    * 
+    * @param myRadius the radius (half the length) of the object in meters.
+    */
+    public void setRadius(double myRadius)
+    {
+        radius = myRadius;
+    }
+    
+    /**
+    * Returns the radius of the object.
+    * 
+    * @return The radius (half the length) of the object in meters.
+    */
+    public double getRadius()
+    {
+        return radius;
+    }
 }
