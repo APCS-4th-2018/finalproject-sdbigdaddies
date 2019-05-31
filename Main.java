16b4bd3c5e31c2d89aa830fabe300d0cd383b232
David Glozman <david.glozman14@gmail.com>
2019-05-31 14:11:57 -0700

    variable
    created local variable for primary stage

---
+++ Main.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class FX here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application 
+{
+    Stage window;
+    Scene scene;
+    Button button;
+
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception 
+    {
+        window = primaryStage;
+        
+        
+
+    }
+}
+
