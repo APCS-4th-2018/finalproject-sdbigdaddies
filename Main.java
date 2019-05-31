b30c141cdc7e06ef153d4607e16be364f261b47d
David Glozman <david.glozman14@gmail.com>
2019-05-31 14:13:14 -0700

    Update Main.java
    set title for initial window

--- Main.java
+++ Main.java
@@ -28,7 +28,7 @@ public class Main extends Application
     public void start(Stage primaryStage) throws Exception 
     {
         window = primaryStage;
-        
+        window.setTitle("Mr. Liao's ultra-fun webquet for torsional pendelums!!");
         
 
     }
