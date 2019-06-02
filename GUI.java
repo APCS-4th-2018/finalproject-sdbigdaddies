e6adc01bb3bff1f3797f69b07c0d504fb20098e5
David Glozman <david.glozman14@gmail.com>
2019-06-02 16:25:48 -0700

    Update GUI.java
    declared local variable stage

--- GUI.java
+++ GUI.java
@@ -13,7 +13,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-public class GUI
+public class GUI extends Application 
 {
+    Stage window;
+    {
     
+    }
 }
