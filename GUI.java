831b8dea7112944639a478a3cffa4c600fd39e88
David Glozman <david.glozman14@gmail.com>
2019-06-02 16:48:07 -0700

    Update GUI.java
    commented code

--- GUI.java
+++ GUI.java
@@ -28,37 +28,48 @@ public class GUI extends Application
         window = primaryStage;
         window.setTitle("Torsional Pendelum Lab");
         
+        //create layout
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setVgap(8);
         grid.setHgap(10);
         
+        //title at the top
         Label nameLabel = new Label("Mr. Liaos Torisonal Pendelum Experience");
         GridPane.setConstraints(nameLabel, 0, 5);
         
+        //Theta Label
         Label initT = new Label("Initial Theta:");
         GridPane.setConstraints(initT, 1, 2);
         
-        //intiial theta Input
+        //Theta Input
         TextField thetaInput = new TextField();
         thetaInput.setPromptText("intial theta value");
         GridPane.setConstraints(thetaInput, 1, 3);
         
+        //Mass Label
         Label mass = new Label("Mass:");
         GridPane.setConstraints(mass, 2, 2);
         
+        //Mass Input
         TextField massInput = new TextField();
         thetaInput.setPromptText("mass value");
         GridPane.setConstraints(massInput, 2, 3);
         
+        //Radius Label
         Label radius = new Label("Radius:");
         GridPane.setConstraints(radius, 3, 2);
         
+        //Radius Input
         TextField radiusInput = new TextField();
         thetaInput.setPromptText("radius value");
         GridPane.setConstraints(massInput, 3, 3);
         
+        //physical objects choices
         ChoiceBox<String> choiceBox = new ChoiceBox<>();
         choiceBox.getItems().addAll("Bar", "Rod", "Disk", "Sphere");
+       
+        //simulation button
+        Button create = new 
     }
 }
