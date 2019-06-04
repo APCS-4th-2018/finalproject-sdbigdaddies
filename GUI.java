2de6922e1c02080f03438c9451ac80c382ffbd89
David Glozman <david.glozman14@gmail.com>
2019-06-03 21:41:45 -0700

    Update GUI.java
    rod input method

--- GUI.java
+++ GUI.java
@@ -146,5 +146,78 @@ public class GUI extends Application
         window.show();
     }
     
-    private
+    private void rodIn()
+    {
+        layout = new VBox(15);
+        layout.setPadding(new Insets(20, 20, 20, 20));
+       
+        HBox row1 = new HBox(5);
+        HBox row2 = new HBox(5);
+        HBox row3 = new HBox(5);
+        HBox row4 = new HBox(5);
+        HBox row5 = new HBox(5);
+        
+        //title at the top
+        Label nameLabel = new Label("Rod");
+       
+        //Theta Label
+        Label initT = new Label("Initial Theta:");
+       
+        //Theta Input
+        TextField thetaInput = new TextField();
+        thetaInput.setPromptText("intial theta value");
+        
+        //creates row one
+        row1.getChildren().addAll(initT, thetaInput);
+        
+        //Mass Label
+        Label mass = new Label("Mass:");
+       
+        //Mass Input
+        TextField massInput = new TextField();
+        massInput.setPromptText("mass value");
+        
+        //creates row 2
+        row2.getChildren().addAll(mass, massInput);
+        
+        //Radius Label
+        Label radius = new Label("Radius:");
+        
+        //Radius Input
+        TextField radiusInput = new TextField();
+        radiusInput.setPromptText("radius value");
+        
+        //creates row 3
+        row3.getChildren().addAll(radius, radiusInput);
+        
+        //Torsional Constant 
+        Label tConstant = new Label("Torsional Constant:");
+        
+        //Torsional Constant input
+        TextField tConstantInput = new TextField();
+        tConstantInput.setPromptText("torsional constant value");
+        
+        //creates row 4
+        row5.getChildren().addAll(tConstant, tConstantInput);
+        
+        //Height constant input
+        Label height = new Label("Height:");
+         
+        //Height input
+        TextField heightInput = new TextField();
+        heightInput.setPromptText("height value");
+        
+        //creates row 5
+        row5.getChildren().addAll(height, heightInput);
+        
+        //simulation button
+        Button create = new Button("Enter");
+        
+        
+        layout.getChildren().addAll(nameLabel, row1, row2, row3, row4, row5, create);
+        
+        Scene rodScene = new Scene(layout, 350, 400);
+        window.setScene(rodScene);
+        window.show();
+    }
 }
